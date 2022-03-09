73f06c62a914f1a930ce1b744ef758c50a4871f0
Elad Sapir <elad.1999.1999@gmail.com>
2022-03-09 13:48:07 +0200

    1-ex2,3 - machine epsilon and fix

--- asm1-3.py
+++ asm1-3.py
@@ -1,1 +1,15 @@
-print(abs(3.0*(4.0/3.0-1)-1))
+"""ex2"""
+def machineEpsilon(func=float):
+    machine_epsilon = func(1)
+    while func(1) + machine_epsilon != func(1):
+        machine_epsilon_last = machine_epsilon
+        machine_epsilon = func(machine_epsilon) / func(2)
+    return machine_epsilon_last
+
+print(machineEpsilon())
+
+"""ex3"""
+x = abs(3.0 * (4.0 / 3.0 - 1) - 1)
+if x - machineEpsilon() <= 0:
+    x=0
+print(x)
