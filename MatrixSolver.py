37ecc9af6ccdc9124c3e1d6e4447bc2c3d3ca3d1
shaiMatz <shyshir@gmail.com>
2022-03-17 13:41:10 +0200

    solver works without vector and printing

--- MatrixSolver.py
+++ MatrixSolver.py
@@ -1,5 +1,5 @@
 def matrixMul(matrixA, matrixB):
-    if lenI(matrixA)==len(matrixA[0]) and len(matrixB)==len(matrixB[0]) and len(matrixA)==len(matrixB):
+    if len(matrixA) == len(matrixA[0]) and len(matrixB) == len(matrixB[0]) and len(matrixA) == len(matrixB):
         matrixC = [[0 for x in range(len(matrixA))] for y in range(len(matrixB))]
         for i in range(len(matrixC)):
             for j in range(len(matrixC)):
@@ -8,6 +8,23 @@ def matrixMul(matrixA, matrixB):
         return matrixC
     else:
         print("Not NxN / same size")
+
+
+def machineEpsilon(func=float):
+    machine_epsilon = func(1)
+    machine_epsilon_last = 0.0
+    while func(1) + machine_epsilon != func(1):
+        machine_epsilon_last = machine_epsilon
+        machine_epsilon = func(machine_epsilon) / func(2)
+    return machine_epsilon_last
+
+
+def I_matrix(A):
+    matrixC = [[0 for x in range(len(A))] for y in range(len(A))]
+    for i in range(len(A)):
+        matrixC[i][i] = 1
+    return matrixC
+
 
 """
 X = [[12, 7, -4],
@@ -48,8 +65,58 @@ def determinant_recursive(A, total=0):
     return total
 
 
-
 def matrixSolve(matrix):
-    if lenI(matrix) == len(matrix) and determinant_recursive(matrix)!=0:
-        solve=[[0 for x in range(len(matrix))] for y in range(len(matrix))]
+    elementary = []
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            exit(0)
+    if determinant_recursive(matrix) != 0:
+        solve = I_matrix(matrix)
+        for i in range(len(matrix)):
+            if matrix[i][i] == 0.0 or abs(matrix[i][i]) - machineEpsilon() < 0.0:
+                k = i
+                while k < len(matrix) and matrix[k][i] == 0:
+                    k += 1
+                temp = solve[i]
+                solve[i] = solve[k]
+                solve[k] = temp
+                elementary.append(solve)
+                matrix = matrixMul(solve, matrix)
+            if matrix[i][i] != 1:
+                solve = I_matrix(matrix)
+                solve[i][i] = 1 / matrix[i][i]
+                elementary.append(solve)
+                matrix = matrixMul(solve, matrix)
+            m = i + 1
+            while m < len(matrix):
+                if matrix[m][i] != 0 or abs(matrix[m][i])-machineEpsilon()<0:
+                    solve = I_matrix(matrix)
+                    solve[m][i] = -matrix[m][i] / matrix[i][i]
+                    elementary.append(solve)
+                    matrix = matrixMul(solve, matrix)
+                m = m + 1
+        n = len(matrix) - 1
+        while n >= 0:
+            m = n - 1
+            while m >= 0:
+                if matrix[m][n] != 0 or abs(matrix[m][n])-machineEpsilon()<0:
+                    solve = I_matrix(matrix)
+                    solve[m][n] = -matrix[m][n] / matrix[n][n]
+                    elementary.append(solve)
+                    matrix = matrixMul(solve, matrix)
+                m = m - 1
+            n = n - 1
 
+    return matrix
+
+
+def printMatrix(matrix):
+    for i in matrix:
+        print('\t'.join(map(str, i)))
+
+
+X = [[12, 7, -4,1],
+     [4, 5, 6,1],
+    [4, 3, 6,4],
+     [7, 8, 9,3]]
+printMatrix(matrixSolve(X))
