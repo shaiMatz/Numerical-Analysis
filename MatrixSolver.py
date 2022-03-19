e326c0a5cc6d477c15beb5fa3035d0bd387c22df
TomerRaitsis <tomerrs81096@gmail.com>
2022-03-19 13:49:44 +0200

    solver works, print for vector and matrix works, error handling works

--- MatrixSolver.py
+++ MatrixSolver.py
@@ -12,14 +12,10 @@ def matrixMul(matrixA, matrixB):
     if len(matrixA) == len(matrixB) and len(matrixA) == len(matrixA[0]):  # checks the sizes of the both matrices
         for i in range(len(matrixA)):  # checks if the left matrix is square
             if len(matrixA) != len(matrixA[i]):
-                exit(0)
-            else:
-                print("Not NxN")
+                raise "Not NxN"
         for i in range(len(matrixB)):  # checks if the left matrix is square
             if len(matrixB) != len(matrixB[i]):
-                exit(0)
-            else:
-                print("Not NxN")
+                raise "Not NxN"
         matrixC =[[0 for x in range(len(matrixA))] for y in range(len(matrixB))]  # Generating a new matrix for the result
         for i in range(len(matrixC)):  # multiplication according to the multiplication algorithm
             for j in range(len(matrixC)):
@@ -27,7 +23,7 @@ def matrixMul(matrixA, matrixB):
                     matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
         return matrixC
     else:
-        print("Not NxN / same size")
+        raise "Not NxN / same size"
 
 
 def matrix_vectorMul(matrixA, vecA):
@@ -52,7 +48,13 @@ def matrix_vectorMul(matrixA, vecA):
     return newVec
 
 
-def machineEpsilon(func=float):
+def machineEpsilon(func=float):  # This is a function from assignment 1
+    """
+    A recursive function that calculates the machine epsilon
+
+    :param func: A parameter that is a function, default if float
+    :return: The machine epsilon
+    """
     machine_epsilon = func(1)
     machine_epsilon_last = 0.0
     while func(1) + machine_epsilon != func(1):
@@ -62,6 +64,12 @@ def machineEpsilon(func=float):
 
 
 def I_matrix(A):
+    """
+    A function that generates a I matrix
+
+    :param A: A matrix, used to make the I matrix the same size the parameter matrix
+    :return: I matrix in the same size as the given matrix (parameter)
+    """
     matrixC = [[0 for x in range(len(A))] for y in range(len(A))]
     for i in range(len(A)):
         matrixC[i][i] = 1
@@ -86,11 +94,24 @@ for i in range(3):
 
 
 def copy_matrix(A):
+    """
+    A function that copy and return an identical matrix
+
+    :param A: A matrix to copy
+    :return: The new copied matrix
+    """
     matrixC = [[A[y][x] for x in range(len(A))] for y in range(len(A))]
     return matrixC
 
 
 def determinant_recursive(A, total=0):
+    """
+    A recursive function that calculates the determinant of a given matrix
+
+    :param A: A matrix
+    :param total: A parameter used in the function that hold the current total, it's default is zero
+    :return: The determinant of matrix A
+    """
     indices = list(range(len(A)))
     if len(A) == 2 and len(A[0]) == 2:
         val = A[0][0] * A[1][1] - A[1][0] * A[0][1]
@@ -108,18 +129,25 @@ def determinant_recursive(A, total=0):
 
 
 def matrixSolve(matrix, vecB):
-    elementary = []
-    matrixes=[]
+    """
+    A function that finds the solution of a systems of linear equations
+
+    :param matrix: A matrix that represent the values of the experiment
+    :param vecB: A vector that represent the results of the experiment
+    :return: A solution vector
+    """
+    elementary = []  # A List for all of the elementary matrices
+    matrixes = []  # A List for all of the mid matrices along the solution
     matrixes.append(matrix)
-    for i in range(len(matrix)):
-        if len(matrix) != len(matrix[i]) or len(matrix)!=len(vecB):
+    for i in range(len(matrix)):  # Checks if the matrix and the vector are in a size that we can work with
+        if len(matrix) != len(matrix[i]) or len(matrix) != len(vecB):
             raise "not NxN or vector is not the same size of matrix"
-    if determinant_recursive(matrix) != 0:
+    if determinant_recursive(matrix) != 0:  # Checks if there is a single solution according to the determinant
         solve = I_matrix(matrix)
-        for i in range(len(matrix)):
-            if matrix[i][i] == 0.0 or abs(matrix[i][i]) - machineEpsilon() < 0.0:
+        for i in range(len(matrix)):  # Applying Gauss Elimination
+            if matrix[i][i] == 0.0 or abs(matrix[i][i]) - machineEpsilon() < 0.0:  # In case the pivot is 0 or a very small number
                 k = i
-                while k < len(matrix) and matrix[k][i] == 0:
+                while k < len(matrix) and matrix[k][i] == 0:  # finding the first raw that does not have a 0 ( in the current column )
                     k += 1
                 temp = solve[i]
                 solve[i] = solve[k]
@@ -127,14 +155,14 @@ def matrixSolve(matrix, vecB):
                 elementary.append(solve)
                 matrix = matrixMul(solve, matrix)
                 matrixes.append(matrix)
-            if matrix[i][i] != 1:
+            if matrix[i][i] != 1:  # In case the pivot in the current raw is not 1
                 solve = I_matrix(matrix)
                 solve[i][i] = 1 / matrix[i][i]
                 elementary.append(solve)
                 matrix = matrixMul(solve, matrix)
                 matrixes.append(matrix)
             m = i + 1
-            while m < len(matrix):
+            while m < len(matrix):  # Turning to zero all the rows below ( in the current column )
                 if matrix[m][i] != 0 or abs(matrix[m][i]) - machineEpsilon() < 0:
                     solve = I_matrix(matrix)
                     solve[m][i] = -matrix[m][i] / matrix[i][i]
@@ -143,9 +171,9 @@ def matrixSolve(matrix, vecB):
                     matrixes.append(matrix)
                 m = m + 1
         n = len(matrix) - 1
-        while n >= 0:
+        while n >= 0:  # Obtaining Solution by Back Substitution
             m = n - 1
-            while m >= 0:
+            while m >= 0:  # Turning to zero all the rows above ( in the current column )
                 if matrix[m][n] != 0 or abs(matrix[m][n]) - machineEpsilon() < 0:
                     solve = I_matrix(matrix)
                     solve[m][n] = -matrix[m][n] / matrix[n][n]
@@ -154,7 +182,7 @@ def matrixSolve(matrix, vecB):
                     matrixes.append(matrix)
                 m = m - 1
             n = n - 1
-        for i in elementary:
+        for i in elementary:  # Multiplying all the elementary matrices with the vector ( to get the solution vector )
             vecB = matrix_vectorMul(i, vecB)
 
         return vecB
@@ -162,20 +190,46 @@ def matrixSolve(matrix, vecB):
         raise "Error"
 
 def printMatrix(matrix):
-    for i in matrix:
-        print('{: ^20} {: ^20} {: ^20} {: ^20}\n'.format(*i))
+    """
+    A function that prints the matrix
+
+    :param matrix: A matrix to print
+    :return:  No return value
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print('{: ^20}'.format(matrix[i][j]), end="")
+        print('\n')
+
+
+def printVector(vec):
+    """
+    A function that prints a vector
+
+    :param vec: Vector to print
+    :return: No return value
+    """
+    for i in vec:
+        print(i)
+
 
 def Norm_Of_A_Matrix(matrix):
+    """
+    A function that calculates a norm of a matrix
+
+    :param matrix: A matrix
+    :return: The value of the norm
+    """
     temp = [0 for x in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             temp[i] += abs(matrix[i][j])
     return max(temp)
 
-X = [[0, 0, 0,1],
-     [0, 5.65165165166516516651651651651, 1,0],
-    [0, -3, 0,0],
-     [1,0,1,0]
+X = [[1, 5, 7,1],
+     [0, 5.6516, 1,4],
+    [9, -3, 5,7],
+     [4,7,5,1]
 ]
-#print(matrixSolve(X, [1, 2, 3,4]))
 printMatrix(X)
+"""matrixSolve()"""
