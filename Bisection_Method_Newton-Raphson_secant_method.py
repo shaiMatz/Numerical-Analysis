33a13191164e9d3877c11bbe7764c56ca5b115fa
TomerRaitsis <tomerrs81096@gmail.com>
2022-05-13 15:34:53 +0300

    secant method added

--- Bisection_Method_Newton-Raphson_secant_method.py
+++ Bisection_Method_Newton-Raphson_secant_method.py
@@ -37,7 +37,8 @@ def bisect(func, start_point, end_point, tol):
 
 ##############newton raphson######################
 
-def newtonRaphson(f, start_point, end_point, TOL, N):
+def newtonRaphson(f, start_point, end_point, TOL):
+    N = 100
     x_r = (start_point + end_point) / 2.0
     s = start_point
     e = end_point
@@ -64,3 +65,148 @@ def newtonRaphson(f, start_point, end_point, TOL, N):
 
         x_r = x_r1
     return "Method failed after {} iterations".format(N), -1, -1
+
+
+#################Secant method######################
+def secant(f, start_point, end_point, TOL):
+    N = 100
+    x_r = end_point
+    x_r0 = start_point
+    for i in range(0, N):
+        x_r1 = (x_r0 * f(x_r) - x_r * f(x_r0)) / (f(x_r) - f(x_r0))
+        if abs(x_r1 - x_r) < TOL:
+            return "Secant method soln: x = " + str(
+                x_r1) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
+                i + 1, start_point, end_point), 1, x_r1
+        x_r0 = x_r
+        x_r = x_r1
+
+    return "The method failed after {} iterations".format(N), -1, -1
+
+
+def choice():
+    return fast_real(input("Enter starting point: ")), fast_real(input("Enter ending point: ")), fast_real(
+        input("Enter tolerance: "))
+
+
+#################main######################
+
+
+def main():
+    while (True):
+        menu = input(
+            "Press 1 for Bisection \nPress 2 for Newton Raphson \nPress 3 for Secant method (anything else to exit):\nchoice:  ")
+        print()
+        if menu == "1":
+            flag = -1
+            flag2 = -1
+            start_point, end_point, TOL = choice()
+            if start_point > end_point:
+                start_point, end_point = end_point, start_point
+            e_p, s_p = round(start_point + 0.1, 4), start_point
+            while s_p < end_point:
+                if func(s_p) * func(e_p) < 0:
+                    message, result, mid = bisect(func, s_p, e_p, TOL)
+                    if result == 1:
+                        flag = 1
+                        print()
+                        print(message)
+                        print()
+                if funcd(s_p) * funcd(e_p) <= 0:
+                    message, result, mid = bisect(funcd, s_p, e_p, TOL)
+                    if result == 1:
+                        if func(mid) == 0:
+                            flag2 = 1
+                            print()
+                            print("The result came from the derivative:\n" + message)
+                            print()
+                e_p = round(e_p + 0.1, 4)
+                if e_p > end_point:
+                    e_p = end_point
+                s_p = round(s_p + 0.1, 4)
+            if flag == -1 and flag2 == -1:
+                print()
+                print("No result for each range in size 0.1 between the starting point {0} to ending point {1}".format(
+                    s_p, e_p))
+                print()
+        elif menu == "2":
+            flag = -1
+            flag2 = -1
+            start_point, end_point, TOL = choice()
+            if start_point > end_point:
+                start_point, end_point = end_point, start_point
+            e_p, s_p = round(start_point + 0.1, 4), start_point
+            while s_p < end_point:
+                if func(s_p) * func(e_p) < 0:
+                    message, result, mid = newtonRaphson(f, s_p, e_p, TOL)
+                    if result == 1:
+                        flag = 1
+                        print()
+                        print(message)
+                        print()
+                if funcd(s_p) * funcd(e_p) <= 0:
+                    message, result, mid = newtonRaphson(f, s_p, e_p, TOL)
+                    if result == 1:
+                        if func(mid) == 0:
+                            flag2 = 1
+                            print()
+                            print("The result came from the derivative:\n" + message)
+                            print()
+                e_p = round(e_p + 0.1, 4)
+                if e_p > end_point:
+                    e_p = end_point
+                s_p = round(s_p + 0.1, 4)
+            if flag == -1 and flag2 == -1:
+                print()
+                print("No result for each range in size 0.1 between the starting point {0} to ending point {1}".format(
+                    s_p, e_p))
+                print()
+        elif menu == "3":
+            flag = -1
+            flag2 = -1
+            mid1 = -1
+            start_point, end_point, TOL = choice()
+            if start_point > end_point:
+                start_point, end_point = end_point, start_point
+            e_p, s_p = round(start_point + 0.1, 4), start_point
+            res = {}
+            while s_p < end_point:
+                if func(s_p) * func(e_p) <= 0:
+                    message, result, mid = secant(func, s_p, e_p, TOL)
+                    if result == 1:
+                        if mid not in res.keys():
+                            res[mid] = message
+                            print()
+                            print(message)
+                            print()
+                        flag = 1
+                e_p = round(e_p + 0.1, 4)
+                if e_p > end_point:
+                    e_p = end_point
+                s_p = round(s_p + 0.1, 4)
+            if flag == -1 and flag2 == -1:
+                print()
+                print("No result for each range in size 0.1 between the starting point {0} to ending point {1}".format(
+                    s_p, e_p))
+                print()
+        else:
+            break
+
+
+x = sp.symbols('x')
+# f = x ** 4 + x ** 3 - 3 * x ** 2
+# f = x ** 3 - x - 1
+f = x**2 -x
+func = lambdify(x, f)
+funcd = f.diff(x)
+funcd = lambdify(x, funcd)
+main()
+
+# f = "x**3-x-1"
+
+
+# print(bisect(f, start_point, end_point, TOL))
+#
+# print(newtonRaphson(f, start_point, end_point, 0.001, 100))
+
+# print(secant(f, start_point, end_point, 0.001, 100))
