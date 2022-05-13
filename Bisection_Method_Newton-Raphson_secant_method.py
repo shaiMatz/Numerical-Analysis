d8816ebbaa01111f1133f62a85478f8143d0f6f2
TomerRaitsis <tomerrs81096@gmail.com>
2022-05-13 20:22:32 +0300

    Done + Documentation

--- Bisection_Method_Newton-Raphson_secant_method.py
+++ Bisection_Method_Newton-Raphson_secant_method.py
@@ -10,41 +10,61 @@ from sympy.utilities.lambdify import lambdify
 # CONDITIONS: a < b, f(a)*f(b) < 0
 # OUTPUT: value which differs from a root of f(x)=0 by less than 'tol'
 def bisect(func, start_point, end_point, tol):
+    """
+    This function finds a root between two points (start and end points) according to the bisection method
+
+
+    :param func: Function
+    :param start_point: start point
+    :param end_point: end point
+    :param tol: tolerance of the solution
+    :return: returns 3 variables: a message, a flag if succeeded (1/-1), the result (or -1 if there is no result)
+    """
     # switch low and high if low is larger than high
-    N = ((math.log(tol / (end_point - start_point), math.e)) / -math.log(2, math.e))
+    N = ((math.log(tol / (end_point - start_point), math.e)) / -math.log(2,
+                                                                         math.e))  # calculating the amount of iterations
     N = ceil(N)
     s = start_point
     e = end_point
     for i in range(0, N):
         mid = (end_point + start_point) / 2.0
-        if func(start_point) == 0:
+        if func(start_point) == 0:  # incase the start point is a root
             return "bisect method soln: x = " + str(
                 start_point
             ) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
                 i + 1, s, e), 1, start_point
-        elif func(end_point) == 0:
+        elif func(end_point) == 0:  # incase the end point is a root
             return "bisect method soln: x = " + str(
                 end_point
             ) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
                 i + 1, s, e), 1, end_point
-        elif func(mid) == 0 or (end_point - start_point) / 2.0 < tol:
+        elif func(mid) == 0 or (end_point - start_point) / 2.0 < tol:  # incase the root is in the middle
             return "bisect method soln: x = " + str(
                 mid) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
                 i + 1, s, e), 1, mid
 
         # same sigh
-        if func(mid) * func(start_point) < 0:
+        if func(mid) * func(start_point) < 0:  # in case the root is in the left of the 'mid'
             end_point = mid
         # diff sign
         else:
             start_point = mid
 
-    return "Method failed after {} iterations".format(N), -1, -1
+    return "Method failed after {} iterations".format(N), -1, -1  # no solution found
 
 
 ##############newton raphson######################
 
 def newtonRaphson(f, start_point, end_point, TOL):
+    """
+    This function finds a root between two points (start and end points) according to the newton-Raphson method
+
+    :param f: Function
+    :param start_point: start point
+    :param end_point: end point
+    :param TOL: tolerance of the solution
+    :return: returns 3 variables: a message, a flag if succeeded (1/-1), the result (or -1 if there is no result)
+    """
     N = 100
     x_r = (start_point + end_point) / 2.0
     s = start_point
@@ -54,17 +74,19 @@ def newtonRaphson(f, start_point, end_point, TOL):
     my_f1 = lambdify(x, my_f1)
 
     for i in range(1, N):
-        x_r1 = x_r - (f(x_r) / my_f1(x_r))
-        if start_point < x_r1 < end_point:
-            if func(start_point) == 0:
+        if my_f1(x_r) == 0:  # in case the value of the derivative at x_r is 0
+            return "Method failed after {} iterations".format(N), -1, -1
+        x_r1 = x_r - (f(x_r) / my_f1(x_r))  # calculation according to the newton-Raphson method
+        if start_point < x_r1 <= end_point:  # in case x_r1 is between the start point and end point
+            if func(start_point) == 0:  # in case the value of the function at the start point is 0
                 return "Newton Raphson method soln: x = " + str(
                     start_point) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
                     i, s, e), 1, start_point
-            if func(end_point) == 0:
+            if func(end_point) == 0:  # in case the value of the function at the end point is 0
                 return "Newton Raphson method soln: x = " + str(
                     end_point) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
                     i, s, e), 1, end_point
-            if my_f1(x_r) == 0:
+            if my_f1(x_r) == 0:  # in case the values of the function and the derivative at x_r is 0
                 if f(x_r) == 0:
                     return "Newton Raphson method soln: x = " + str(
                         x_r) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
@@ -75,16 +97,25 @@ def newtonRaphson(f, start_point, end_point, TOL):
                     i, s, e), 1, x_r1
 
         x_r = x_r1
-    return "Method failed after {} iterations".format(N), -1, -1
+    return "Method failed after {} iterations".format(N), -1, -1  # no solution found
 
 
 #################Secant method######################
 def secant(f, start_point, end_point, TOL):
+    """
+    This function finds a root between two points (start and end points) according to the newton-Raphson method
+
+    :param f: Function
+    :param start_point: start point
+    :param end_point: end point
+    :param TOL: tolerance of the solution
+    :return: returns 3 variables: a message, a flag if succeeded (1/-1), the result (or -1 if there is no result)
+    """
     N = 100
     x_r = end_point
     x_r0 = start_point
     for i in range(0, N):
-        x_r1 = (x_r0 * f(x_r) - x_r * f(x_r0)) / (f(x_r) - f(x_r0))
+        x_r1 = (x_r0 * f(x_r) - x_r * f(x_r0)) / (f(x_r) - f(x_r0))  # calculation according to the secant method
         if abs(x_r1 - x_r) < TOL:
             return "Secant method soln: x = " + str(
                 x_r1) + "\nFound root after {0} iterations between the starting point {1} to ending point {2}".format(
@@ -95,7 +126,7 @@ def secant(f, start_point, end_point, TOL):
     return "The method failed after {} iterations".format(N), -1, -1
 
 
-def choice():
+def choice():  # A functions to get the starting point, ending point and tolerance from the user
     return fast_real(input("Enter starting point: ")), fast_real(input("Enter ending point: ")), fast_real(
         input("Enter tolerance: "))
 
@@ -104,6 +135,14 @@ def choice():
 
 
 def main():
+    """
+    The main method, lets the user to choose a method, starting point, ending point and tolerance.
+    The program will check is there is a suspicious area (f(a) * f(b) < 0) it will use the chosen method to find the root
+    in this range.
+    The program will check the range between the starting point and ending point in leaps of 0.1
+
+    :return: no return value
+    """
     while (True):
         menu = input(
             "Press 1 for Bisection \nPress 2 for Newton Raphson \nPress 3 for Secant method (anything else to exit):\nchoice:  ")
@@ -117,7 +156,7 @@ def main():
                 start_point, end_point = end_point, start_point
             e_p, s_p = round(start_point + 0.1, 4), start_point
             while s_p < end_point:
-                if func(s_p) * func(e_p) <= 0:
+                if func(s_p) * func(e_p) <= 0:  # checks for the function
                     message, result, mid = bisect(func, s_p, e_p, TOL)
                     if result == 1:
                         if mid not in res.keys():
@@ -126,7 +165,7 @@ def main():
                             print(message)
                             print()
                         flag = 1
-                if funcd(s_p) * funcd(e_p) < 0:
+                if funcd(s_p) * funcd(e_p) < 0:  # checks for the derivative
                     message, result, mid = bisect(funcd, s_p, e_p, TOL)
                     if result == 1:
                         if func(mid) == 0:
@@ -154,7 +193,7 @@ def main():
                 start_point, end_point = end_point, start_point
             e_p, s_p = round(start_point + 0.1, 4), start_point
             while s_p < end_point:
-                if func(s_p) * func(e_p) <= 0:
+                if func(s_p) * func(e_p) <= 0:   # checks for the function
                     message, result, mid = newtonRaphson(f, s_p, e_p, TOL)
                     if result == 1:
                         if mid not in res.keys():
@@ -163,7 +202,7 @@ def main():
                             print(message)
                             print()
                         flag = 1
-                if funcd(s_p) * funcd(e_p) <= 0:
+                if funcd(s_p) * funcd(e_p) < 0: # checks for the derivative
                     message, result, mid = newtonRaphson(f, s_p, e_p, TOL)
                     if result == 1:
                         if func(mid) == 0:
@@ -192,7 +231,7 @@ def main():
             e_p, s_p = round(start_point + 0.1, 4), start_point
             res = {}
             while s_p < end_point:
-                if func(s_p) * func(e_p) <= 0:
+                if func(s_p) * func(e_p) <= 0:  # checks for the function
                     message, result, mid = secant(func, s_p, e_p, TOL)
                     if result == 1:
                         if mid not in res.keys():
@@ -218,9 +257,11 @@ x = sp.symbols('x')
 
 # f = x ** 4 + x ** 3 - 3 * x ** 2
 # f = x ** 3 - 3*x**2
-f = x ** 2 - x
+#f = x ** 3 + 2 * x ** 2 + 1 * x + 5
 # f = x ** 2 - 5*x+2
-#f = x ** 5 + 3 * x ** 4 + x
+# f = x ** 5 + 3 * x ** 4 + x
+#f = sp.cos(x)
+f = sp.Abs(5)
 
 func = lambdify(x, f)
 funcd = f.diff(x)
